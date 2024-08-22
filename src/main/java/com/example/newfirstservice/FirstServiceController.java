fa1c0c0cc23ff95746432c7040af689d1af404a2
sungjoong <stylealist@gmail.com>
2024-08-22 14:30:20 +0900

    [feat] 기초 설정 및 welcome, message endpoint 추가

--- src/main/java/com/example/newfirstservice/FirstServiceController.java
+++ src/main/java/com/example/newfirstservice/FirstServiceController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Hello World in First Service";
     }
 }
