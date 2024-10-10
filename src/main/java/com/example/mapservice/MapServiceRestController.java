7f959257cb6f7dabc247d98bf9d9433d0cb0ef5e
sungjoong <stylealist@gmail.com>
2024-10-10 19:47:26 +0900

    [test] jenkins 자동배포 테스트

--- src/main/java/com/example/mapservice/MapServiceRestController.java
+++ src/main/java/com/example/mapservice/MapServiceRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 테스트
 @RestController
 //@RequestMapping("/map")
 @RequestMapping("/")
