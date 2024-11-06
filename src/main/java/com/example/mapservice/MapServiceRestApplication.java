df6d7a182735984f946b9058eabe6f3aa470191f
sungjoong <stylealist@gmail.com>
2024-11-06 18:58:20 +0900

    [feat] 서버 실행 시 등록된 Bean이 출력되는 코드 추가

--- src/main/java/com/example/mapservice/MapServiceRestApplication.java
+++ src/main/java/com/example/mapservice/MapServiceRestApplication.java
@@ -2,12 +2,19 @@ package com.example.mapservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MapServiceRestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MapServiceRestApplication.class, args);
+
+        ApplicationContext applicationContext = SpringApplication.run(MapServiceRestApplication.class, args);
+
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for(String beanName : allBeanNames){
+            System.out.println(beanName);
+        }
     }
 
 }
