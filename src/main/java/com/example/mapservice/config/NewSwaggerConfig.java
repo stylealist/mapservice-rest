61eac6301e3bd1e903d251aefebd4261b814c7c8
sungjoong <stylealist@gmail.com>
2024-11-07 20:27:28 +0900

    [study] swagger api 추가

---
+++ src/main/java/com/example/mapservice/config/NewSwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.mapservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@OpenAPIDefinition(
+        info = @Info(title = "My Restful Service API 명세서",
+                     description = "SpringBoot로 개발하는 RESTful API 명세서 입니다.",
+                     version = "v1.0.0")
+)
+@Configuration
+@RequiredArgsConstructor
+public class NewSwaggerConfig {
+    @Bean
+    public GroupedOpenApi customTestOpenApi(){
+        String[] paths = {"/users/**","/admin/**"};
+
+        return GroupedOpenApi.builder()
+                .group("일반사용자와 관리자를 위한 User 도메인에 대한 API")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
--- src/main/java/com/example/mapservice/controller/UserController.java
+++ src/main/java/com/example/mapservice/controller/UserController.java
@@ -3,6 +3,11 @@ package com.example.mapservice.controller;
 import com.example.mapservice.bean.User;
 import com.example.mapservice.dao.UserDaoService;
 import com.example.mapservice.exception.UserNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -17,6 +22,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@Tag(name = "user-controller", description = "일반 사용자 서비스를 위한 컨트롤러입니다.")
 public class UserController {
     private UserDaoService userDaoService;
 
@@ -29,8 +35,16 @@ public class UserController {
         return userDaoService.findAll();
     }
 
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용해서 사용자 상세 정보 조회를 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "USER NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
     @GetMapping("/users/{id}")
-    public EntityModel<User> retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(
+            @Parameter(description = "사용자 ID",required = true, example = "1") @PathVariable int id) {
         User user = userDaoService.findOne(id);
 
         if(user == null){
