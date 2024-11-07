095db442017deb660f8abd27c8b94072675350b0
sungjoong <stylealist@gmail.com>
2024-11-07 13:39:41 +0900

    [study] Path-variable 학습코드

--- src/main/java/com/example/mapservice/MapServiceRestApplication.java
+++ src/main/java/com/example/mapservice/MapServiceRestApplication.java
@@ -8,13 +8,15 @@ import org.springframework.context.ApplicationContext;
 public class MapServiceRestApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(MapServiceRestApplication.class, args);
+        
+        /* 생성되는 빈을 모두 확인 할 수 있는 코드
         ApplicationContext applicationContext = SpringApplication.run(MapServiceRestApplication.class, args);
 
         String[] allBeanNames = applicationContext.getBeanDefinitionNames();
         for(String beanName : allBeanNames){
             System.out.println(beanName);
-        }
+        }*/
     }
 
 }
---
+++ src/main/java/com/example/mapservice/bean/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.mapservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+//    public HelloWorldBean(String message) {
+//        this.message = message;
+//    }
+}
