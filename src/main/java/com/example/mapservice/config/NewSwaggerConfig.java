9c670fdd3b45583735175616e7cd533071c6e40b
sungjoong <stylealist@gmail.com>
2024-11-08 10:43:28 +0900

    [test] swagger api 사용을 위한 reverse-proxy 테스트 코드

--- src/main/java/com/example/mapservice/config/NewSwaggerConfig.java
+++ src/main/java/com/example/mapservice/config/NewSwaggerConfig.java
@@ -2,10 +2,15 @@ package com.example.mapservice.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
 @OpenAPIDefinition(
         info = @Info(title = "My Restful Service API 명세서",
                      description = "SpringBoot로 개발하는 RESTful API 명세서 입니다.",
@@ -22,5 +27,11 @@ public class NewSwaggerConfig {
                 .group("일반사용자와 관리자를 위한 User 도메인에 대한 API")
                 .pathsToMatch(paths)
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("/api/map")));
     }
 }
